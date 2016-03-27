b4644db6734c01234d404603e5835169e7178f68
Wes White <wtealwhite@gmail.com>
2016-03-27 11:00:11 -0500

    remove Navigation folder (unused)

--- src/components/Navigation/About.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Links from './src/component/Links';
-
-const About = () => <div><h1>About</h1><Links /></div>;
-
-export default About;
--- src/components/Navigation/Contact.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Links from './src/component/Links';
-
-const Contact = () => <div><h1>Contact</h1><Links /></div>;
-
-export default Contact;
